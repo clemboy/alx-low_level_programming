9f606d8d9fc3e170f8706d4355e694ffc5bbb660
clemboy <clementtshepo3@gmail.com>
2022-03-13 10:29:07 -0700

    meet

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#incluse <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char)0;
+printf("Size of an int: %d byte(s)\", sizeof(int));clear
